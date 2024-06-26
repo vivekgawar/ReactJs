76ae9093326b2255241fc1fbaf57cb05ce859fd2
Vivek Gawar <vivekgaver2016@gmail.com>
2024-06-27 02:41:20 +0530

    Project CleanUp and making with React and React+Vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Nah fam i aint learning react</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return(
+  <h1>Vite & React is cool</h1>
+  )
+}
+
+export default App
